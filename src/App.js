8442d874053725b92bf306d7b7504c34e674f11f
KrishnaA01 <mahato77@hotmail.com>
2022-01-30 23:44:39 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./input.css"
 import ImageCard from './components/ImageCard'
+import ImageSearch from './components/ImageSearch'
 import React, {useState, useEffect} from 'react';
 
 
@@ -20,11 +21,15 @@ function App() {
     })
     .catch(err => console.log(err))
 
-  })
+  }, [term]);
 
 
   return (
    <div className="container mx-auto">
+    <ImageSearch  searchText ={(text)=> setTerm(text)} />
+    
+    {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
        {images.map(image => (
          <ImageCard key ={image.id} image={image}/>
